af85e1bcbbaf90528f38698bb8cd0fdc0223f337
Rishikesh Rahi <rishikesh.rahi@tpcentralodisha.com>
2024-04-19 23:44:45 +0530

    Initial Commit

---
+++ mylibraryrahi/src/main/java/com/tpcodl/mylibraryrahi/MySampleClass.java
@@ -0,0 +1,14 @@
+package com.tpcodl.mylibraryrahi;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class MySampleClass {
+
+    public static void main(String[]arr){
+
+    }
+    public static void showToast(Context context){
+        Toast.makeText(context, "Toast from library", Toast.LENGTH_SHORT).show();
+    }
+}
